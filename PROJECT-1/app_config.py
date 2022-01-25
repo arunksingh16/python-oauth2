3c3650cc82292251dc5d880df8f6b893183f4617
Arun Singh <singh@dummy.com>
2022-01-25 15:29:40 +0000

    app.py

---
+++ PROJECT-1/app_config.py
@@ -0,0 +1,8 @@
+import os
+
+CLIENT_SECRET = "clientsecret" 
+AUTHORITY = "https://login.microsoftonline.com/tenantname.onmicrosoft.com"
+CLIENT_ID = "clientid"
+REDIRECT_PATH = "/signin-oidc" 
+SCOPE = []
+SESSION_TYPE = "filesystem"
